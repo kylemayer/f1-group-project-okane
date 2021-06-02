2ffdf5c98a43445a9746f8de27d5d91e9288b88c
Kyle Mayer <x@kylemayer.dev>
2021-06-02 09:58:36 -0700

    created td elements

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,13 +1,25 @@
 import { logout, getCurrentUser } from '../local-storage.js';
 import { getTotalMoney, getTotalExpenses } from '../budget.js';
-const logoutButton = document.querySelector('#logout'); 
+import { user1 } from '../assets/demo-data.js';
+
+const logoutButton = document.querySelector('#logout');
+const rootDiv = document.querySelector('#root');
 
 const user = getCurrentUser();
 const currentMonth = user.month;
 const totalMoney = getTotalMoney();
 const totalExpenses = getTotalExpenses();
 
-
 logoutButton.addEventListener('click', () => {
-    logout(); 
-}); 
+    logout();
+});
+
+function renderUserData(user1) {
+    const tr = document.createElement('tr');
+
+    const companyName = document.createElement('td');
+    const categoryName = document.createElement('td');
+    const cashValue = document.createElement('td');
+    const description = document.createElement('td');
+    const subscription = document.createElement('td');
+}
